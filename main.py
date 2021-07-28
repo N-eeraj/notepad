cd8157c560a1dfa3381b859bec2302a37562826a
N-eeraj <neeraj7rajeena@gmail.com>
2021-07-28 23:01:07 +0530

    show file with same name exist

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ while True:
     option = int(input("\nMain Menu\n1. New\n2. Edit\n3. Rename\n4. Delete\n5. Exit\nSelect Option: "))
     if option == 1:
         file_name = "files/" +  input("Enter filename: ") + ".txt"
+        if file_name[6:] in all_files:
+            print("This filename already exists")
+            continue
         new_file = open(file_name, "w")
         content = input("Enter Content: ")
         new_file.write(content)
+        print("File Saved")
         new_file.close()
     elif option == 2:
         pass
