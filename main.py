17ded7f58c4e20e0c361bbcfebdffaa31c3b5f5c
N-eeraj <neeraj7rajeena@gmail.com>
2021-07-28 22:40:00 +0530

    initial set-up

---
+++ main.py
@@ -0,0 +1,15 @@
+while True:
+    option = int(input("\nMain Menu\n1. New\n2. Edit\n3. Rename\n4. Delete\n5. Exit\nSelect Option: "))
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option == 4:
+        pass
+    elif option == 5:
+        print("Exiting...")
+        break
+    else:
+        print("Invalid Option")
