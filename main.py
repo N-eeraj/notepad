3fc72e79e3d66ec9892f475952febc1575118bc2
N-eeraj <neeraj7rajeena@gmail.com>
2021-07-28 22:56:07 +0530

    create files

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from os import listdir
 while True:
+    all_files = listdir("files")
     option = int(input("\nMain Menu\n1. New\n2. Edit\n3. Rename\n4. Delete\n5. Exit\nSelect Option: "))
     if option == 1:
-        pass
+        file_name = "files/" +  input("Enter filename: ") + ".txt"
+        new_file = open(file_name, "w")
+        content = input("Enter Content: ")
+        new_file.write(content)
+        new_file.close()
     elif option == 2:
         pass
     elif option == 3:
