2c6400e2cc75ec47c2f28f739842323a28f3e46e
N-eeraj <neeraj7rajeena@gmail.com>
2021-07-29 12:09:15 +0530

    added save file

--- notepad.py
+++ notepad.py
@@ -1,24 +1,35 @@
 from tkinter import Tk, Menu, Text, messagebox
 from tkinter.constants import END
+from tkinter.simpledialog import askstring
 
-
-# funtion to create new file
-def newFile():
-    save_file = askSave()
-    if save_file == True:
-        saveFile()
-    elif save_file == None:
-        return
-    text_area.delete(1.0, END)
+# function to get typed text
+def getText():
+    return text_area.get(1.0, END)
 
 # function to ask if to save file
 def askSave():
     if len(getText()) > 1:
         return messagebox.askyesnocancel("Save File", "Do you want to save the file before exiting?")       
 
-# function to get typed text
-def getText():
-    return text_area.get(1.0, END)
+# funtion to create new file
+def newFile():
+    save_file = askSave()
+    if save_file == True:
+        if not saveFile():
+            return
+    elif save_file == None:
+        return
+    text_area.delete(1.0, END)
+
+def saveFile():
+    filename = askstring("Save File", "Enter filename")
+    if filename != None:
+        filename += ".txt"
+        file = open("files/" + filename, "w")
+        file.writelines(getText())
+        messagebox.showinfo("Saved", "The file has been saved")
+        return True
+    return False
 
 # function to toggle darkmode
 def darkmode():
@@ -34,7 +45,8 @@ def exitNotepad():
     if messagebox.askyesno("Close Window", "Are you sure you want to exit?"):
         save_file = askSave()
         if save_file == True:
-            saveFile()
+            if not saveFile():
+                return
         elif save_file == None:
             return
         notepad.destroy()
@@ -49,7 +61,7 @@ menubar = Menu(notepad)
 file = Menu(menubar, tearoff = 0)
 file.add_command(label = "New File", command = newFile)
 file.add_command(label = "Open File")
-file.add_command(label= "Save")
+file.add_command(label= "Save", command = saveFile)
 menubar.add_cascade(label = "File", menu = file)
 menubar.add_command(label = "Dark Mode", command = darkmode)
 menubar.add_command(label = "Exit",  command = exitNotepad)
